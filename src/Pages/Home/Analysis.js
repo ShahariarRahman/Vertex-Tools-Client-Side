644063e2d269684d14737cafff5af60458bf7349
ShahariarRahman <shahariarrahman98@gmail.com>
2022-05-26 23:22:58 +0600

    Analysis Component Added

---
+++ src/Pages/Home/Analysis.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Analysis = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-primary my-52 text-center'>Empty Extra Section 1</h1>
+        </div>
+    );
+};
+
+export default Analysis;
