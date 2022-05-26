28e3550bde82bc8c7822efaa8c1866befc579b3d
ShahariarRahman <shahariarrahman98@gmail.com>
2022-05-26 23:24:21 +0600

    Blogs and My Portfolio Created

---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>Blogs</h2>
+            <p>14.1 How will you improve the performance of a React Application?</p>
+            <p>14.2 What are the different ways to manage a state in a React application?(5-7)(line 2.max 10 line).</p>
+            <p>  14.3 How does prototypical inheritance work in JavaScript?</p>
+            <p> 14.4 Why you do not set the state directly in React.For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts.</p>
+            <p>  14.5 You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</p>
+            <p>14.6 What is a unit test? Why should write unit tests?</p>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyPortfolio = () => {
+    return (
+        <div>
+            <h2>My Portfolio</h2>
+            <p>1. name</p>
+            <p>2. email address</p>
+            <p>3. educational background</p>
+            <p> 4. list of technologies or skills you have as a web developer.</p>
+            <p> 5. Add links of three of your best projects (live website links).</p>
+        </div>
+    );
+};
+
+export default MyPortfolio;
